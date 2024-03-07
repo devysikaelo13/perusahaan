d97bcb9797e6737d287fcaf5e542a449cdcbbbac
devysikaelo13 <138899761+devysikaelo13@users.noreply.github.com>
2024-03-07 18:38:34 +0700

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Perusahaan
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form_Menu());
+            //Application.Run(new Form_Tambah_Pegawai());
+            //Application.Run(new Form_Tambah_Departemen());
+            //Application.Run(new Form_Tambah_Proyek());
+            //Application.Run(new Form_Penugasan());
+            //Application.Run(new Form_View_Pegawai());
+        }
+    }
+}
